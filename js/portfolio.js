6d276026509310aea7a870ce56bdff62fd3a32c1
J Kim <janice.juyeon.kim@gmail.com>
2018-10-10 12:59:56 -0400

    Minor tweaks

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,14 +1,14 @@
 var colors = ["#02b3e4", "#2d3c49", "#7d97ad"];
 
-var works = [{"work-title":"Asobibox", "work-url":"http://www.asobibox.com/", "work-img-src":"./img/work/asobibox.png", "work-img-alt":"Asobibox"},
-             {"work-title":"CCQL", "work-url":"https://github.com/dibbs-vdc/ccql/", "work-img-src":"./img/work/ccql.png", "work-img-alt":"DIBBS VDC CCQL"},
-             {"work-title":"Foobar", "work-url":"https://github.com/shabububu/foobar2016/", "work-img-src":"./img/work/foobar.png", "work-img-alt":"Google Foobar Challenge"}];
+var works = [{"work-title":"Asobibox", "work-url":"http://www.asobibox.com/", 
+              "work-img-src":"./img/work/asobibox.png", "work-img-alt":"Asobibox"},
+             {"work-title":"CCQL", "work-url":"https://github.com/dibbs-vdc/ccql/", 
+              "work-img-src":"./img/work/ccql.png", "work-img-alt":"DIBBS VDC CCQL"},
+             {"work-title":"Foobar", "work-url":"https://github.com/shabububu/foobar2016/", 
+              "work-img-src":"./img/work/foobar.png", "work-img-alt":"Google Foobar Challenge"}];
 
 function displayPalette() {
-  console.log( "Displaying colors: " + colors );
-
   const palette = $( '#color-palette' );
-
   for (var i = 0; i < colors.length; i++) {
     color = colors[i];
     square = $( '<div />', { class: 'row-2 square', 
@@ -21,22 +21,20 @@ function displayPalette() {
 }
 
 function displayFeaturedWork() {
-  console.log( "Displaying featured works: " + works );
-
-  const worksList = $( '#works-list' );
-
+  var worksList = $( '#works-list' );
   for (var i = 0; i < works.length; i++) {
     work = works[i];
+
     workImg = $( '<img />', { src: work["work-img-src"],
-                              alt: work["work-img-alt"],
-                              width: "200", height: "150" });
+                              alt: work["work-img-alt"] });
     workTitle = $( '<h3 />', { class: "featured-site-title",
                                text: work["work-title"] });
     workLink = $( '<p />' ).append( $( '<a />', { href: work["work-url"],
                                                   text: work["work-url"] }));
+
     singleWork = $( '<div />', { class: 'featured-site' });
     singleWork.append( workImg ).append( workTitle ).append( workLink );
-    colWrapper = $( '<div />', { class: 'col-sm' });
+    colWrapper = $( '<div />', { class: 'col-md-4' });
     worksList.append( colWrapper.append( singleWork ));
   }
 }
